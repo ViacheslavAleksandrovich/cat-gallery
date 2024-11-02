930acd6e953a1b757d49f73dfcb2d4b0b5426d97
viacheslav <ksm05012000@gmail.com>
2024-11-02 17:02:53 +0200

    feat: add background style

--- src/App.tsx
+++ src/App.tsx
@@ -34,29 +34,31 @@ const App: React.FC = () => {
     : filteredCats;
 
   return (
-    <div className="max-w-7xl mx-auto p-4">
-      <header className="text-center mb-8">
-        <h1 className="text-4xl font-bold text-gray-800">Cats Gallery</h1>
-        <p className="text-lg text-gray-600">A collection of adorable cats</p>
-      </header>
+    <div className="bg-gradient-to-r from-blue-400 to-purple-500 min-h-screen flex justify-center items-center">
+      <div className="max-w-7xl mx-auto p-4 ">
+        <header className="text-center mb-8">
+          <h1 className="text-4xl font-bold text-gray-800">Cats Gallery</h1>
+          <p className="text-lg text-gray-600">A collection of adorable cats</p>
+        </header>
 
-      <Filter
-        breeds={breeds}
-        selectedBreed={selectedBreed}
-        onBreedChange={setSelectedBreed}
-        showFavorites={showFavorites}
-        onToggleShowFavorites={() => setShowFavorites(!showFavorites)}
-      />
+        <Filter
+          breeds={breeds}
+          selectedBreed={selectedBreed}
+          onBreedChange={setSelectedBreed}
+          showFavorites={showFavorites}
+          onToggleShowFavorites={() => setShowFavorites(!showFavorites)}
+        />
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {displayedCats.map((cat) => (
-          <CatCard
-            key={cat.id}
-            cat={cat}
-            isFavorite={favorites.includes(cat.id)}
-            onToggleFavorite={toggleFavorite}
-          />
-        ))}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {displayedCats.map((cat) => (
+            <CatCard
+              key={cat.id}
+              cat={cat}
+              isFavorite={favorites.includes(cat.id)}
+              onToggleFavorite={toggleFavorite}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
